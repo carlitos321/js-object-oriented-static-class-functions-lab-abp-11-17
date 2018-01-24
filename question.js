72c08134d41183d2af0acaae9fadbe0669055b99
carlitos321 <ca.cabrera321@gmail.com>
2018-01-24 21:43:07 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,23 @@
 'use strict';
+
+class Question {
+
+  constructor(content){
+    this.content = content
+    this.save()
+  }
+
+  save() {
+    this.constructor._All.push(this)
+  }
+
+  static Find(id) {
+    return this.All()[id-1]
+  }
+
+  static All(){
+    return this._All;
+  }
+}
+
+Question._All = []
